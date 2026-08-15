f2659f2e4531eed7c7182ab7396b9297d4f05b3a
Abhiraaj R Chandrasekaran <94893084+neox-d@users.noreply.github.com>
2026-08-15 22:25:53 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        if (m != n) return false;
+
+        int[] freq = new int[26];
+
+        for (int i=0; i<m; i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int val: freq) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
